14fa3a0adaf51ea17fd328be8db7691b629a8996
egrizq <rizq.ramadhan17@gmail.com>
2024-04-10 18:10:16 +0700

    change file name

---
+++ helpers/convertDate.go
@@ -0,0 +1,16 @@
+package helpers
+
+import (
+	"log"
+	"time"
+)
+
+func ConvertDate(dateStr string) time.Time {
+	dateFormat := "02/01/2006"
+	date, err := time.Parse(dateFormat, dateStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return date
+}
